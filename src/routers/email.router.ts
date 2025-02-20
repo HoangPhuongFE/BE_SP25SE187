e8c08424ce03d38e7285272ff2b17d609a7ae80d
Nguyen Phuong Hoang <hoang093898xxx@gmail.com>
2025-02-21 00:40:48 +0700

    fix send-mail 2

--- src/routers/email.router.ts
+++ src/routers/email.router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { EmailTemplateController } from "../controller/emailTemplate.controller";
 import { EmailController } from "../controller/email.controller";
+import { authenticateToken, checkRole } from "../middleware/user.middleware";
 
 const router = Router();
-const emailTemplateController = new EmailTemplateController();
 const emailController = new EmailController();
 
-router.get("/email-templates", emailTemplateController.getAllTemplates);
-router.get("/email-templates/:id/params", emailTemplateController.getTemplateParams);
-router.post("/email/send", emailController.sendEmails);
+
+router.post("/email/send",
+   authenticateToken,
+  checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer"]),
+   emailController.sendEmails);
 
 export default router;
--- src/routers/emailTemplate.router.ts
+++ src/routers/emailTemplate.router.ts
@@ -1,19 +1,38 @@
 import { Router } from "express";
 import { EmailTemplateController } from "../controller/emailTemplate.controller";
+import { authenticateToken, checkRole } from "../middleware/user.middleware";
 
 const router = Router();
 const emailTemplateController = new EmailTemplateController();
 
-router.get("/templates", emailTemplateController.getAllTemplates);
+router.get("/templates",
+    authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer"]),
+ emailTemplateController.getAllTemplates);
 
-router.get("/templates/:id", emailTemplateController.getTemplateById);
+router.get("/templates/:id",
+    authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer",]), 
+    emailTemplateController.getTemplateById);
 
-router.get("/templates/:id/params", emailTemplateController.getTemplateParams);
+router.get("/templates/:id/params"
+    ,authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer",]),
+     emailTemplateController.getTemplateParams);
 
-router.post("/templates", emailTemplateController.createTemplate);
+router.post("/templates",
+     authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer",]),
+    emailTemplateController.createTemplate);
 
-router.put("/templates/:id", emailTemplateController.updateTemplate);
+router.put("/templates/:id",
+     authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer",]),
+    emailTemplateController.updateTemplate);
 
-router.delete("/templates/:id", emailTemplateController.deleteTemplate);
+router.delete("/templates/:id",
+    authenticateToken,
+    checkRole(['admin', "graduation_thesis_manager", "examination_officer", "academic_officer",]),
+    emailTemplateController.deleteTemplate);
 
 export default router;
