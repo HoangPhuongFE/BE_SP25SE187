0eb741876608ec6a2edb3c9ea9bfc8ea0edefb88
hoangphuong <hoang093898xxx@gmail.com>
2025-03-06 22:10:12 +0700

    bug : user-semester-roles

--- src/routers/user-semester-role.routes.ts
+++ src/routers/user-semester-role.routes.ts
@@ -9,28 +9,28 @@ const semesterRoleController = new SemesterRoleController();
 router.get(
   '/',
   authenticateToken,
-  checkRole(['admin', 'academic_officer',"graduation_thesis_manager", "examination_officer",'mentor','lecturer'], false),
+  checkRole(['admin', 'academic_officer',"graduation_thesis_manager", "examination_officer",'mentor','lecturer']),
   semesterRoleController.getRolesBySemester.bind(semesterRoleController)
 );
 
 router.post(
   '/',
   authenticateToken,
-  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"], false),
+  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"]),
   semesterRoleController.createSemesterRole.bind(semesterRoleController)
 );
 
 router.put(
   '/',
   authenticateToken,
-  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"], false),
+  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"]),
   semesterRoleController.updateSemesterRole.bind(semesterRoleController)
 );
 
 router.delete(
   '/:userId/:roleId/:semesterId',
   authenticateToken,
-  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"], false),
+  checkRole(['admin','academic_officer',"graduation_thesis_manager", "examination_officer"]),
   semesterRoleController.deleteSemesterRole.bind(semesterRoleController)
 );
 
