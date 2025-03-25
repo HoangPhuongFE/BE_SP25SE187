6c20cd41ba6bde800e57440f78a898b947d87278
hoangphuong <hoang093898xxx@gmail.com>
2025-03-26 00:32:14 +0700

    fix db

--- src/routers/year.route.ts
+++ src/routers/year.route.ts
@@ -3,7 +3,7 @@ import { YearController } from "../controller/year.controller";
 
 const router = express.Router();
 const yearController = new YearController();
-
+//
 router.get("/", yearController.getAllYears.bind(yearController));
 router.post("/", yearController.createYear.bind(yearController));
 router.put("/:id", yearController.updateYear.bind(yearController));
